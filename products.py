9f1062960fd11d7ab4d97a1e5f925b3705317190
shiny <unique590284@gmail.com>
2023-02-07 00:48:49 +0800

    encoding

--- products.py
+++ products.py
@@ -10,3 +10,12 @@ print(products) #印出大清單
 for p in products:
 	print(p) #印出每一個小清單
 	print(p[0], '的價格是', p[1])
+
+# 'abc' + '123' = 'abc123' 字串可以合併
+#'abc' * 3 = 'abcabcabc'
+
+with open('products.csv', 'w', encoding = 'utf-8') as f: #針對打開的檔案要進行編寫的動作
+	f.write('商品,價格\n')
+	for p in products:
+		f.write(p[0] + ',' + p[1] + '\n') #進行寫入_四個字串合併 '\n':空格以示換行 ',':要用逗點在excel檔才是分開兩隔
+
